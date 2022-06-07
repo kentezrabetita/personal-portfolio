6afc3cbdb22fa79d1208e67c11074b60ec890412
kentezrabetita <kentezrabetita@gmail.com>
2022-06-07 16:03:50 +0800

    add dark mode

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Navbar from './navbar';
 
 export default function Layout({ children }) {
   return (
-    <div className='pb-6 dark:bg-neutral-900'>
+    <div className='pb-6 transition dark:bg-neutral-900 '>
       <Navbar />
       {children}
     </div>
--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useTheme } from 'next-themes';
 
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
-  const [theme, setTheme] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setIsMounted(true), []);
+  if (!isMounted) return null;
+
   return (
-    <nav className='p-6 md:p-10 md:px-40 text-md dark:text-white'>
+    <nav className='container p-6 mx-auto md:p-10 text-md dark:text-white'>
       <div className='flex flex-row items-center justify-between'>
         <div>
           <h1 className='md:text-lg'>
@@ -14,7 +20,7 @@ export default function Navbar() {
             </Link>
           </h1>
         </div>
-        <div className='flex-row hidden space-x-20 text-right md:flex'>
+        <div className='flex-row hidden space-x-10 text-right md:flex'>
           <div>
             <Link href='/about'>
               <a>about.</a>
@@ -30,6 +36,29 @@ export default function Navbar() {
               <a>contact.</a>
             </Link>
           </div>
+          <div>
+            {theme === 'dark' ? (
+              <button
+                onClick={() => {
+                  setTheme(theme === 'dark' ? 'light' : 'dark');
+                  setShowMenu(!showMenu);
+                }}
+                className='px-6 text-black bg-white rounded-md'
+              >
+                light.
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  setTheme(theme === 'dark' ? 'light' : 'dark');
+                  setShowMenu(!showMenu);
+                }}
+                className='px-6 text-white bg-black rounded-md '
+              >
+                dark.
+              </button>
+            )}
+          </div>
         </div>
         <div className='md:hidden'>
           <button className='menu-btn' onClick={() => setShowMenu(!showMenu)}>
@@ -40,17 +69,27 @@ export default function Navbar() {
       {showMenu && (
         <div className='md:hidden'>
           <div className='fixed z-10 top-0 flex bg-white flex-col items-center self-end justify-center py-8 space-y-6 dark:bg-neutral-900 text-black dark:text-white h-[100vh] left-0 right-0'>
-            <Link href='/'>
-              {theme ? (
-                <a className='p-2 px-6 text-black bg-white rounded-md'>
-                  light mode.
-                </a>
-              ) : (
-                <a className='p-2 px-6 text-white bg-black rounded-md'>
-                  dark mode.
-                </a>
-              )}
-            </Link>
+            {theme === 'dark' ? (
+              <button
+                onClick={() => {
+                  setTheme(theme === 'dark' ? 'light' : 'dark');
+                  setShowMenu(!showMenu);
+                }}
+                className='p-2 px-6 text-black bg-white rounded-md'
+              >
+                light.
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  setTheme(theme === 'dark' ? 'light' : 'dark');
+                  setShowMenu(!showMenu);
+                }}
+                className='p-2 px-6 text-white bg-black rounded-md'
+              >
+                dark.
+              </button>
+            )}
             <Link href='/about'>
               <a>about.</a>
             </Link>
