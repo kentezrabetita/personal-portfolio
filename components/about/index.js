c8ddeaafcc5b781cdbd09c98178ed9504df4d606
kentezrabetita <kentezrabetita@gmail.com>
2022-06-08 20:56:57 +0800

    add social links

--- components/about/index.js
+++ components/about/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function AboutComponent() {
   return (
@@ -23,34 +24,34 @@ export default function AboutComponent() {
         </div>
         <div className='flex flex-col space-y-4'>
           <div>Few technologies I've work with : </div>
-          <div className='flex flex-col'>
+          <div className='flex flex-col cursor-default'>
             <div className='flex flex-col'>
               <div>
                 Frontend<span className='text-lg text-orange-400'>.</span>
               </div>
               <div className='flex flex-row flex-wrap p-4 text-justify'>
-                <div className='p-1 m-1 text-white bg-orange-600 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-orange-600 rounded-lg hover:scale-105'>
                   HTML
                 </div>
-                <div className='p-1 m-1 text-white bg-blue-700 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-blue-700 rounded-lg hover:scale-105'>
                   CSS
                 </div>
-                <div className='p-1 m-1 text-white bg-yellow-500 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-yellow-500 rounded-lg hover:scale-105'>
                   JavaScript
                 </div>
-                <div className='p-1 m-1 text-white rounded-lg bg-violet-600'>
+                <div className='p-1 m-1 text-white transition ease-in-out rounded-lg hover:scale-105 bg-violet-600'>
                   Bootstrap
                 </div>
-                <div className='p-1 m-1 text-white bg-blue-600 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-blue-600 rounded-lg hover:scale-105'>
                   Ant Design
                 </div>
-                <div className='p-1 m-1 text-white rounded-lg bg-sky-500'>
+                <div className='p-1 m-1 text-white transition ease-in-out rounded-lg hover:scale-105 bg-sky-500'>
                   Tailwind
                 </div>
-                <div className='p-1 m-1 text-white rounded-lg bg-sky-500'>
+                <div className='p-1 m-1 text-white transition ease-in-out rounded-lg hover:scale-105 bg-sky-500'>
                   React.js
                 </div>
-                <div className='p-1 m-1 text-white bg-gray-700 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-gray-700 rounded-lg hover:scale-105'>
                   Next.js
                 </div>
               </div>
@@ -60,19 +61,19 @@ export default function AboutComponent() {
                 Backend<span className='text-lg text-sky-400'>.</span>
               </div>
               <div className='flex flex-row flex-wrap p-4 text-justify'>
-                <div className='p-1 m-1 text-white bg-green-600 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-green-600 rounded-lg hover:scale-105'>
                   Node.js
                 </div>
-                <div className='p-1 m-1 text-white bg-orange-600 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-orange-600 rounded-lg hover:scale-105'>
                   Hapi.js
                 </div>
-                <div className='p-1 m-1 text-white bg-blue-500 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-blue-500 rounded-lg hover:scale-105'>
                   MySQL
                 </div>
-                <div className='p-1 m-1 text-white rounded-lg bg-sky-600'>
+                <div className='p-1 m-1 text-white transition ease-in-out rounded-lg hover:scale-105 bg-sky-600'>
                   PostgreSQL
                 </div>
-                <div className='p-1 m-1 text-white rounded-lg bg-gradient-to-r from-pink-500 to-yellow-500'>
+                <div className='p-1 m-1 text-white transition ease-in-out rounded-lg hover:scale-105 bg-gradient-to-r from-pink-500 to-yellow-500'>
                   REST APIs
                 </div>
               </div>
@@ -82,16 +83,16 @@ export default function AboutComponent() {
                 Tools<span className='text-lg text-red-600'>.</span>
               </div>
               <div className='flex flex-row flex-wrap p-4 text-justify'>
-                <div className='p-1 m-1 text-white bg-blue-600 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-blue-600 rounded-lg hover:scale-105'>
                   VSCode
                 </div>
-                <div className='p-1 m-1 text-white rounded-lg bg-zinc-700'>
+                <div className='p-1 m-1 text-white transition ease-in-out rounded-lg bg-zinc-700 hover:scale-105'>
                   Figma
                 </div>
-                <div className='p-1 m-1 text-white bg-yellow-500 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-yellow-500 rounded-lg hover:scale-105'>
                   Navicat
                 </div>
-                <div className='p-1 m-1 text-white bg-orange-600 rounded-lg'>
+                <div className='p-1 m-1 text-white transition ease-in-out bg-orange-600 rounded-lg hover:scale-105'>
                   Postman
                 </div>
               </div>
@@ -102,29 +103,44 @@ export default function AboutComponent() {
               Links<span className='text-lg text-yellow-400'>.</span>
             </div>
             <div className='flex flex-row flex-wrap p-4 text-justify'>
-              <button className='p-1 m-1 text-white bg-blue-600 rounded-lg'>
-                facebook
-              </button>
-              <button className='p-1 m-1 text-white bg-blue-600 rounded-lg'>
-                linkedin
-              </button>
-              <button className='p-1 m-1 text-white rounded-lg bg-zinc-700'>
-                github
-              </button>
-              <button className='p-1 m-1 text-white bg-gray-500 rounded-lg'>
-                dev.to
-              </button>
-              <button className='p-1 m-1 text-white rounded-lg bg-gradient-to-r from-pink-500 to-orange-500'>
-                instagram
-              </button>
+              <Link href='https://facebook.com/kentdagrit'>
+                <a target='_blank'>
+                  <button className='p-1 m-1 text-white transition ease-in-out bg-blue-600 rounded-lg hover:scale-105'>
+                    facebook
+                  </button>
+                </a>
+              </Link>
+              <Link href='https://www.linkedin.com/in/kent-ezra-betita-64355a212/'>
+                <a target='_blank'>
+                  <button className='p-1 m-1 text-white transition ease-in-out bg-blue-600 rounded-lg hover:scale-105'>
+                    linkedin
+                  </button>
+                </a>
+              </Link>
+              <Link href='https://github.com/kentezrabetita/'>
+                <a target='_blank'>
+                  <button className='p-1 m-1 text-white transition ease-in-out rounded-lg bg-zinc-700 hover:scale-105'>
+                    github
+                  </button>
+                </a>
+              </Link>
+              <Link href='https://dev.to/kentezrabetita'>
+                <a target='_blank'>
+                  <button className='p-1 m-1 text-white transition ease-in-out bg-gray-500 rounded-lg hover:scale-105'>
+                    dev.to
+                  </button>
+                </a>
+              </Link>
+              <Link href='https://www.instagram.com/kentezrathegreat/?hl=en'>
+                <a target='_blank'>
+                  <button className='p-1 m-1 text-white transition ease-in-out rounded-lg bg-gradient-to-r from-pink-500 to-orange-500 hover:scale-105'>
+                    instagram
+                  </button>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
       </div>
     </div>
   );
