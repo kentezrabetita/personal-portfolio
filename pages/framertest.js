772240a30de054899340cf1c98d14a985d94fc9b
kentezrabetita <kentezrabetita@gmail.com>
2022-06-11 13:58:14 +0800

    remove unused component

--- pages/framertest.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import Hero from '../components/landing/hero';
-import Layout from '../components/layout';
-import Footer from '../components/landing/footer';
-
-export default function framertest() {
-  return <Layout></Layout>;
-}
